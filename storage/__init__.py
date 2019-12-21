e0ab35113e1363388161345cb39d87aaa37c3348
Kieran Bacon <kieran.bacon@outlook.com>
2019-12-21 02:18:58 +0000

    add a find manager util
    provide utility function to find the manager classes themselves.
    Code reuse to reduce connect function code.
    add find to the imports

--- storage/__init__.py
+++ storage/__init__.py
@@ -1,6 +1,6 @@
 sep = '/'
 
 from .artefacts import File, Directory
-from .utils import connect
+from .utils import find, connect
 from .sync import Sync
 from .backup import Backup
--- storage/utils.py
+++ storage/utils.py
@@ -5,16 +5,7 @@ from .interfaces import Manager
 sep = '/'
 MANAGERS = {}
 
-def connect(name: str, *, config=None, manager: str = None, **kwargs) -> Manager:
-
-    if config is None and manager is None:
-        raise ValueError("Need to specify a config or a manager")
-
-    if config is not None:
-        # Load the contents of the config and assign it to a variable
-        manager = None
-        kwargs = {}
-
+def find(manager) -> Manager:
     # Get the manager class for the manager type given - load the manager type if not already loaded
     if manager in MANAGERS:
         mClass = MANAGERS[manager]
@@ -27,6 +18,21 @@ def connect(name: str, *, config=None, manager: str = None, **kwargs) -> Manager
 
         else:
             raise ValueError("Couldn't find a manager called '{}'".format(manager))
+
+    return mClass
+
+def connect(name: str, *, config=None, manager: str = None, **kwargs) -> Manager:
+
+    if config is None and manager is None:
+        raise ValueError("Need to specify a config or a manager")
+
+    if config is not None:
+        # Load the contents of the config and assign it to a variable
+        manager = None
+        kwargs = {}
+
+    # Find the class for the manager
+    mClass = find(manager)
 
     # Create the Manager - pass all the kwarg arguments
     return mClass(name = name, **kwargs)
