b3895c6dc40211d158cc3371627adf4cd16d4326
Kieran Bacon <kieran.bacon@outlook.com>
2019-12-21 02:21:04 +0000

    add initial work for backup CLI

--- storage/backup.py
+++ storage/backup.py
@@ -3,7 +3,7 @@ import better
 
 from .interfaces import Manager
 from .sync import Sync
-from .utils import connect
+from .utils import find, connect
 
 CONFIG_DIRECTORY = os.path.expanduser(os.path.join('~', '.backup_configs'))
 if not os.path.exists(CONFIG_DIRECTORY): os.mkdir(CONFIG_DIRECTORY)
@@ -60,3 +60,40 @@ class BackupManager:
     @classmethod
     def main(cls):
         pass
+
+    
+    def createBackupScript(self, name):
+
+        print('Creating a new backup process')
+
+        managers = []
+        for dest in ['local', 'remote']:
+            while True:
+                mType = input('Specify the {} manager type: '.format(dest))
+
+                try:
+                    # Find the manager class to initialise
+                    mClass = find(mType)
+
+                    # Load that classes CLI
+                    managers.append(mClass.CLI())
+                    break
+
+                except ValueError:
+                    print("Couldn't find a manager with that name. please try again.\n")
+
+        # Select the backup policy
+        while True:
+            policy = input('Select a backup policy(ACCEPT_1, ACCEPT_2, STOP, CONFLICT): ')
+
+            if policy not in ("ACCEPT_1", "ACCEPT_2", "STOP", "CONFLICT"):
+                print("Invalid selection. please try again.")
+
+            break
+
+        # Create the backup process and save its details
+        Backup(name, *managers, policy=policy).save()
+
+    def runSync(self, name):
+        Backup.load(name).sync()
+            
