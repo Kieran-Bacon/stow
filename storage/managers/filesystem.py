f904d2ca81c725253bd323ab56a22ff0c8f7c05a
Kieran Bacon <kieran.bacon@outlook.com>
2020-03-15 23:01:29 +0000

    update manager to load items just in time rather than indexing the filesystem during initialisation
    overhaul of the manager functions
    requirement for two new abstract methods to be implemented which in turn meant tha the filesystem was updated as a test
    aftefact directories now needed to record whether they have had all of their contents loaded, which is vitial for listing directories

--- storage/managers/filesystem.py
+++ storage/managers/filesystem.py
@@ -8,7 +8,7 @@ import contextlib
 from .. import SEP
 from ..artefacts import Artefact, File, Directory
 from ..manager import LocalManager
-from ..utils import connect
+from .. import exceptions
 
 class FS(LocalManager):
     """ Wrap a local filesystem (a networked drive or local directory)
@@ -39,6 +39,16 @@ class FS(LocalManager):
     def _dirname(self, artefact):
         _, path = self._artefactFormStandardise(artefact)
         return os.path.dirname(path)
+
+    def _isdir(self, relpath: str):
+
+        abspath = self._abspath(relpath)
+
+        if not os.path.exists(abspath):
+            raise exceptions.ArtefactNotFound("Could not find an artefact at location: {}".format(relpath))
+
+        else:
+            return os.path.isdir(abspath)
 
     def _makefile(self, path) -> File:
         abspath = self._abspath(path)
@@ -97,6 +107,17 @@ class FS(LocalManager):
         os.makedirs(os.path.dirname(absDestination), exist_ok=True)
         os.rename(self._abspath(srcObj.path), absDestination)
 
+    def _listdir(self, relpath: str):
+
+        abspath = self._abspath(relpath)
+
+        dirs, files = set(), set()
+        for art in os.listdir(abspath):
+            if os.path.isdir(os.path.join(abspath, art)):   dirs.add(self._join(relpath,art))
+            else:                                           files.add(self._join(relpath,art))
+
+        return dirs, files
+
     def _rm(self, artefact: Artefact, path: str):
 
 
@@ -110,67 +131,3 @@ class FS(LocalManager):
 
     def toConfig(self):
         return {'manager': 'FS', 'path': self._path}
-
-
-class Locals(LocalManager):
-
-    def __init__(self, name, directories):
-        super().__init__(name)
-
-        # Unpack all the directories and keep references to the original managers
-        directories = [os.path.expanduser(d) for d in directories]
-        self._default = directories[0].split(os.path.sep)[-1]
-        self._namesToPaths = {d.split(os.path.sep)[-1]: os.path.abspath(d) for d in directories}
-        self._managers = {name: connect(name, manager='FS', path=path) for name, path in self._namesToPaths.items()}
-
-    def refresh(self):
-        for manager in self._managers.values():
-            manager.refresh()
-
-    def paths(self, artefactType = None):
-        # Set up the paths for the manager
-        return {
-            "{sep}{}{sep}{}".format(name, path.strip(SEP), sep=SEP): art
-            for name, manager in self._managers.items()
-            for path, art in manager.paths().items()
-            if artefactType is None or isinstance(art, artefactType)
-        }
-
-    @ staticmethod
-    def _splitFilepath(filepath: str) -> (str, str):
-        nodes = filepath.strip(SEP).split(SEP)
-        return nodes[0], SEP + SEP.join(nodes[1:])
-
-    def __getitem__(self, filepath: str):
-        d, path = self._splitFilepath(filepath)
-        if d not in self._managers:
-            return self._managers[self._default][filepath]
-        return self._managers[d][path]
-
-    def __contains__(self, filepath: str):
-        if isinstance(filepath, Artefact): return super().__contains__(filepath)
-        d, path = self._splitFilepath(filepath)
-        if d not in self._managers:
-            return filepath in self._managers[self._default]
-        return path in self._managers[d]
-
-
-    def get(self, src_remote: str, dest_local):
-        source_path = super().get(src_remote, dest_local)
-        d, path = self._splitFilepath(source_path)
-        if d not in self._managers:
-            return self._managers[self._default].get(source_path, dest_local)
-        return self._managers[d].get(path, dest_local)
-
-    def put(self, src_local: str, dest_remote):
-        with super().put(src_local, dest_remote) as (source_path, destination_path):
-            d, path = self._splitFilepath(destination_path)
-
-            if d not in self._managers:
-                return self._managers[self._default].put(source_path, destination_path)
-            return self._managers[d].put(source_path, path)
-
-    def rm(self, filename, recursive: bool = False):
-        path = super().rm(filename, recursive)
-        d, path = self._splitFilepath(path)
-        return self._managers[d].rm(path, recursive)
