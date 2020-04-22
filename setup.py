9af5cf5e8af99d964625ed3c1af26c0abcd9babc
Kieran Bacon <kieran.bacon@outlook.com>
2020-04-22 12:05:13 +0100

    update version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(os.path.join(os.path.dirname(__file__), 'requirements.txt')) as fh:
 setup(
     name='storage',
     install_requires=requires,
-    version="0.0.5",
+    version="0.0.5.1",
     description="",
 
     author="Kieran Bacon",
