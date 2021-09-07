e7efaf5e6938eacdb1d45a276d5a42d5b1396d32
Kieran Bacon <kieran.bacon@outlook.com>
2021-09-07 22:31:11 +0000

    remove personal encryption key adjustment from setup.sh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,6 +1,3 @@
 pip install --upgrade pip
 pip install --user -e .
 pip install -r .devcontainer/requirements.txt
-
-mkdir -p /home/vscode/Documents/Encryption-keys
-cp /home/kieran/ngenius/Encryption-keys/ngeniusadmin.pem /home/vscode/Documents/Encryption-keys/ngeniusadmin.pem
